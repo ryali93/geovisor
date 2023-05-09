9b89cda82507ad34337a440b1c5cf1484bcf53d4
Roy Yali <ryali93@gmail.com>
2023-05-09 13:27:21 +0200

    Configured to run on a local network

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -16,9 +16,16 @@ export class MapComponent implements OnInit{
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
         maxZoom: 18, 
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    })
+      }),
+      L.tileLayer.wms('http://212.128.192.252:8080/geoserver/ne/wms', {
+        layers: 'ne:gpo_countries',
+        format: 'image/png',
+        transparent: true,
+        version: '1.1.0',
+      })
     ],
     zoom: 5,
     center: L.latLng(-10, -75)
   };
+
 }
