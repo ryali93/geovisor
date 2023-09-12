535d44cc3bb1bf4cf196df03c6b0951b6b42ce98
Roy Yali <ryali93@gmail.com>
2023-09-12 14:49:55 +0200

    json file updated | combobox basic selectors finished

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -8,76 +8,107 @@ const map = new ol.Map({
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([0, 0]),
-      zoom: 2
+      center: ol.proj.fromLonLat([-4.7240425, 39.7825346]),
+      zoom: 7
     })
   });
 
 //////////////////////////////////////////////////////////////////////
-// Example of data to fill the comboboxes with autocomplete
-const data = {
-"COPERNICUS": {
-    "S2_SR_HARMONIZED": {
-    "bands": ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B9", "B10", "B11", "B12", "B2A", "QA10", "QA20", "QA60"],
-    },
-    "S2_TOA": {
-    "bands": ["B1", "B10", "B11", "B12", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B9", "QA10", "QA20", "QA60"],
-    }
-},
-"LANDSAT": {
-    "LANDSAT_8_C1": {
-    "bands": ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BQA"],
-    }
-}
-};
-
-// Get elements from the DOM
-const missionSelect = document.getElementById('missionSelect');
-const satelliteSelect = document.getElementById('satelliteSelect');
-const bandSelect = document.getElementById('bandSelect');
-
-// Fill the first combobox with the missions
-Object.keys(data).forEach(mission => {
-  const option = document.createElement('option');
-  option.value = mission;
-  option.textContent = mission;
-  missionSelect.appendChild(option);
+// Toggle advanced options
+document.getElementById('toggleAdvanced').addEventListener('click', function() {
+  // this.classList.toggle('active');
+  const advancedOptions = document.getElementById('advancedOptions');
+  if (advancedOptions.style.display === 'none' || advancedOptions.style.display === '') {
+    advancedOptions.style.display = 'block';
+  } else {
+    advancedOptions.style.display = 'none';
+  }
 });
 
-// Event for when a mission is selected
-missionSelect.addEventListener('change', function() {
-  const selectedMission = this.value;
+//////////////////////////////////////////////////////////////////////
+// Example of data
+// const data = {
+// "COPERNICUS": {
+//     "S2_SR_HARMONIZED": {
+//     "bands": ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B9", "B10", "B11", "B12", "B2A", "QA10", "QA20", "QA60"],
+//     },
+//     "S2_TOA": {
+//     "bands": ["B1", "B10", "B11", "B12", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B9", "QA10", "QA20", "QA60"],
+//     }
+// },
+// "LANDSAT": {
+//     "LANDSAT_8_C1": {
+//     "bands": ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BQA"],
+//     }
+// }
+// };
 
-  // Clear and enable the next combobox
-  satelliteSelect.innerHTML = '<option value="">Select a satellite</option>';
-  satelliteSelect.disabled = false;
+let globalData;
 
-  // Fill the combobox with satellites
-  Object.keys(data[selectedMission]).forEach(satellite => {
+// Load data from JSON file
+fetch('./gee_catalog.json')
+  .then(response => response.json())
+  .then(data => {
+    globalData = data;
+    initializeMissionComboBox(data);
+  })
+  .catch(error => console.error('Error al cargar el archivo JSON:', error));
+
+function initializeMissionComboBox(data) {
+  const missionSelect = document.getElementById('missionSelect');
+  const uniqueMissions = [...new Set(data.map(item => item.MISSION))];  // Get unique missions
+
+  uniqueMissions.forEach(mission => {
     const option = document.createElement('option');
-    option.value = satellite;
-    option.textContent = satellite;
-    satelliteSelect.appendChild(option);
+    option.value = mission;
+    option.textContent = mission;
+    missionSelect.appendChild(option);
   });
-});
 
-// Event for when a satellite is selected
-satelliteSelect.addEventListener('change', function() {
-  const selectedMission = missionSelect.value;
-  const selectedSatellite = this.value;
+  missionSelect.addEventListener('change', function() {
+    const selectedMission = this.value;
+    initializeProductComboBox(selectedMission);
+  });
+}
 
-  // Clear and enable the next combobox
+function initializeProductComboBox(selectedMission) {
+  const productSelect = document.getElementById('productSelect');
+  productSelect.innerHTML = '<option value="">Select product</option>';
+  productSelect.disabled = false;
+
+  const relevantProducts = globalData.filter(item => item.MISSION === selectedMission);
+
+  relevantProducts.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.PRODUCT;
+    option.textContent = product.PRODUCT;
+    productSelect.appendChild(option);
+  });
+
+  productSelect.addEventListener('change', function() {
+    const selectedProduct = this.value;
+    initializeBandComboBox(selectedMission, selectedProduct);
+  });
+}
+
+function initializeBandComboBox(selectedMission, selectedProduct) {
+  const bandSelect = document.getElementById('bandSelect');
   bandSelect.innerHTML = '<option value="">Select bands</option>';
   bandSelect.disabled = false;
 
-  // Fill the combobox with bands
-  data[selectedMission][selectedSatellite].bands.forEach(band => {
-    const option = document.createElement('option');
-    option.value = band;
-    option.textContent = band;
-    bandSelect.appendChild(option);
-  });
-});
+  const relevantProduct = globalData.find(item => item.MISSION === selectedMission && item.PRODUCT === selectedProduct);
+
+  if (relevantProduct && Array.isArray(relevantProduct.band_values)) {
+    relevantProduct.band_values.forEach((band, index) => {
+      const option = document.createElement('option');
+      option.value = relevantProduct.band_values[index] || band;
+      option.textContent = relevantProduct.band_values[index] + " - " + relevantProduct.band_description[index] || band;
+      option.title = relevantProduct.band_description[index] || band;
+
+      bandSelect.appendChild(option);
+    });
+  }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Calendar widget with flatpickr
@@ -85,18 +116,17 @@ flatpickr('#calendar-range', {
     "mode": "range"
 });
 
-
 //////////////////////////////////////////////////////////////////////
-// Primero, define la fuente y la capa vectorial
+// First, define the source and vector layer
 let source = new ol.source.Vector();
 let vector = new ol.layer.Vector({
   source: source
 });
 
-// Añadir la capa al mapa
+// Add the layer to the map
 map.addLayer(vector);
 
-// Definir la función addDrawInteraction
+// Define a function that adds the draw interaction to the map
 let draw;
 function addDrawInteraction(type) {
   if (draw) {
@@ -115,16 +145,13 @@ function addDrawInteraction(type) {
   });
 }
 
-// Añadir escuchadores de eventos para los botones
+// Add event listeners for the buttons
 document.getElementById('drawRectangle').addEventListener('click', function() {
   vector = addDrawInteraction('Rectangle');
 });
 document.getElementById('drawPolygon').addEventListener('click', function() {
   vector = addDrawInteraction('Polygon');
 });
-// document.getElementById('drawCircle').addEventListener('click', function() {
-//   vector = addDrawInteraction('Circle');
-// });
 document.getElementById('drawClear').addEventListener('click', function() {
   source.clear();
 });
@@ -132,34 +159,33 @@ document.getElementById('drawClear').addEventListener('click', function() {
 //////////////////////////////////////////////////////////////////////
 // Get elements from the DOM in a function
 document.getElementById('getDataButton').addEventListener('click', async function() {
-    // Obtener el valor del select de la misión
+    // Get the value of the mission select
     const collectionName = document.getElementById('missionSelect').value;
   
-    // Obtener la geometría dibujada (si existe)
+    // Get the geometry drawn (if exists)
     const features = source.getFeatures();
     let geometry;
     if (features.length > 0) {
       geometry = features[0].getGeometry();
     }
   
-    // Para este ejemplo, asumo que tienes select o input para reducer y resolution
-    const reducer = "mean"//document.getElementById('reducerSelect').value; // reemplaza con tu propio elemento
-    const resolution = 30 //document.getElementById('resolutionInput').value; // reemplaza con tu propio elemento
+    const reducer = "mean"
+    const resolution = 30 
   
-    // Obtener las fechas del calendario Flatpickr
+    // Get the dates from the Flatpickr calendar
     const dateRange = document.getElementById('calendar-range')._flatpickr.selectedDates;
     const startDate = dateRange.length > 0 ? dateRange[0].toLocaleDateString('en-CA') : null;
     const endDate = dateRange.length > 1 ? dateRange[1].toLocaleDateString('en-CA') : null;
   
-    // Obtener las bandas seleccionadas (si se seleccionaron)
+    // Get the selected bands (if any)
     const bandSelect = document.getElementById('bandSelect');
     const selectedOptions = Array.from(bandSelect.selectedOptions);
     const bands = selectedOptions.map(option => option.value);
   
-    // Mostrar en consola
+    // Show in console
     console.log({
       collectionName,
-      geometry: geometry ? geometry.getCoordinates() : null, // o como quieras almacenar la geometría
+      geometry: geometry ? geometry.getCoordinates() : null,
       reducer,
       resolution,
       startDate,
@@ -167,11 +193,11 @@ document.getElementById('getDataButton').addEventListener('click', async functio
       bands
     });
 
-    // Obtener el mapid
+    // Get the mapid
     const mapid = await get_mapid(geometry);
     console.log(mapid);
 
-    // Añadir la imagen XYZ al mapa
+    // Add the XYZ image to the map
     const xyzSource = new ol.source.XYZ({
         url: mapid,
         attributions: "XYZ",
